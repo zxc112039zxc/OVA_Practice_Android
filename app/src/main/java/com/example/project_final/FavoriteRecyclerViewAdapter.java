18a7886e4731839bed420186799669acfd8fecf3
Steve Lee <4515steve@gamil.com>
2022-07-14 20:19:47 +0800

    format modification

--- app/src/main/java/com/example/project_final/FavoriteRecyclerViewAdapter.java
+++ app/src/main/java/com/example/project_final/FavoriteRecyclerViewAdapter.java
@@ -21,15 +21,13 @@ import java.util.Collection;
 
 public class FavoriteRecyclerViewAdapter extends RecyclerView.Adapter<FavoriteRecyclerViewAdapter.ViewHolder> implements Filterable {
 
-    /**上方的arrayList為RecyclerView所綁定的ArrayList*/
     ArrayList<Problem> arrayList;
-    /**儲存最原先ArrayList的狀態(也就是充當回複RecyclerView最原先狀態的陣列)*/
     ArrayList<Problem> arrayListFilter;
 
     public FavoriteRecyclerViewAdapter(ArrayList<Problem> arrayList) {
         this.arrayList = arrayList;
         arrayListFilter = new ArrayList<>();
-        /**這裡把初始陣列複製進去了*/
+
         arrayListFilter.addAll(arrayList);
     }
 
@@ -43,7 +41,7 @@ public class FavoriteRecyclerViewAdapter extends RecyclerView.Adapter<FavoriteRe
             tvNumber = itemView.findViewById(R.id.tvShowProblemNumber);
             tvProblem = itemView.findViewById(R.id.tvShowProblemName);
             btnFavorite = itemView.findViewById(R.id.btnFavoriteShow);
-            /**點擊事件*/
+
             itemView.setOnClickListener(this);
         }
 
@@ -85,31 +83,29 @@ public class FavoriteRecyclerViewAdapter extends RecyclerView.Adapter<FavoriteRe
     public Filter getFilter() {
         return mFilter;
     }
-    /**使用Filter濾除方法*/
+
     Filter mFilter = new Filter() {
-        /**此處為正在濾除字串時所做的事*/
+
         @Override
         protected FilterResults performFiltering(CharSequence constraint) {
-            /**先將完整陣列複製過去*/
+
             ArrayList<Problem> filteredList = new ArrayList<>();
-            /**如果沒有輸入，則將原本的陣列帶入*/
+
             if (constraint == null || constraint.length() == 0 || constraint != "favorite") {
                 filteredList.addAll(arrayListFilter);
             } else {
-                /**如果有輸入，則照順序濾除相關字串
-                 * 如果你有更好的搜尋演算法，就是寫在這邊*/
                 for (Problem problem: arrayListFilter) {
                     if (problem.isFavorite()) {
                         filteredList.add(problem);
                     }
                 }
             }
-            /**回傳濾除結果*/
+
             FilterResults filterResults = new FilterResults();
             filterResults.values = filteredList;
             return filterResults;
         }
-        /**將濾除結果推給原先RecyclerView綁定的陣列，並通知RecyclerView刷新*/
+
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             arrayList.clear();
--- app/src/main/java/com/example/project_final/HistoryRecyclerViewAdapter.java
+++ app/src/main/java/com/example/project_final/HistoryRecyclerViewAdapter.java
@@ -18,15 +18,13 @@ import java.util.Collection;
 
 public class HistoryRecyclerViewAdapter extends RecyclerView.Adapter<HistoryRecyclerViewAdapter.ViewHolder> {
 
-    /**上方的arrayList為RecyclerView所綁定的ArrayList*/
     ArrayList<Problem> arrayList;
-    /**儲存最原先ArrayList的狀態(也就是充當回複RecyclerView最原先狀態的陣列)*/
     ArrayList<Problem> arrayListFilter;
 
     public HistoryRecyclerViewAdapter(ArrayList<Problem> arrayList) {
         this.arrayList = arrayList;
         arrayListFilter = new ArrayList<>();
-        /**這裡把初始陣列複製進去了*/
+
         arrayListFilter.addAll(arrayList);
     }
 
@@ -40,7 +38,7 @@ public class HistoryRecyclerViewAdapter extends RecyclerView.Adapter<HistoryRecy
             tvNumber = itemView.findViewById(R.id.tvShowProblemNumber);
             tvProblem = itemView.findViewById(R.id.tvShowProblemName);
             btnFavorite = itemView.findViewById(R.id.btnFavoriteShow);
-            /**點擊事件*/
+
             itemView.setOnClickListener(this);
         }
 
@@ -82,31 +80,30 @@ public class HistoryRecyclerViewAdapter extends RecyclerView.Adapter<HistoryRecy
 //    public Filter getFilter() {
 //        return mFilter;
 //    }
-//    /**使用Filter濾除方法*/
+
 //    Filter mFilter = new Filter() {
-//        /**此處為正在濾除字串時所做的事*/
+
 //        @Override
 //        protected FilterResults performFiltering(CharSequence constraint) {
-//            /**先將完整陣列複製過去*/
+
 //            ArrayList<Problem> filteredList = new ArrayList<>();
-//            /**如果沒有輸入，則將原本的陣列帶入*/
+
 //            if (constraint == null || constraint.length() == 0) {
 //                filteredList.addAll(arrayListFilter);
 //            } else {
-//                /**如果有輸入，則照順序濾除相關字串
-//                 * 如果你有更好的搜尋演算法，就是寫在這邊*/
+
 //                for (Problem problem: arrayListFilter) {
 //                    if (problem.get(0).toLowerCase().contains(constraint.toString().toLowerCase()) || problem.get(1).toLowerCase().contains(constraint.toString().toLowerCase())) {
 //                        filteredList.add(problem);
 //                    }
 //                }
 //            }
-//            /**回傳濾除結果*/
+
 //            FilterResults filterResults = new FilterResults();
 //            filterResults.values = filteredList;
 //            return filterResults;
 //        }
-//        /**將濾除結果推給原先RecyclerView綁定的陣列，並通知RecyclerView刷新*/
+
 //        @Override
 //        protected void publishResults(CharSequence constraint, FilterResults results) {
 //            arrayList.clear();
